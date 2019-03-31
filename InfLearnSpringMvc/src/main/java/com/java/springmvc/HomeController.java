f9088432418574cb6b1fa99e073c0002461f7546
SeoYeon Hwang <syhwang125@gmail.com>
2019-03-31 16:48:16 +0900

    spring mvc project added

---
+++ InfLearnSpringMvc/src/main/java/com/java/springmvc/HomeController.java
@@ -0,0 +1,42 @@
+package com.java.springmvc;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ * InfLearn Spring 강좌에서 11번째 스프MVC 기초 
+ * 애플리케이션 실행하고 브라우저에서 http://localhost:8080/springmvc 
+ * 
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";    // view jsp name 
+	}
+	
+}
